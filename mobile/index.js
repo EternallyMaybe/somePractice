33d4d6fd2ed1dc6e8665c18c1276c0847bef9326
licheng5 <licheng5@xiaomi.com>
2019-07-15 17:09:08 +0800

    移动端适配

--- mobile/index.js
+++ mobile/index.js
@@ -1,3 +1,14 @@
-window.addEventListener('DOMContentLoaded', function() {
-    console.log(document.documentElement.clientWidth);
-})
+(function (doc, win) {
+    if (!doc.addEventListener) return;
+
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 12 * (clientWidth / 375) + 'px';
+        };
+        
+    recalc();
+    win.addEventListener(resizeEvt, recalc, false);
+})(document, window);
